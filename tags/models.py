ebaf1d9ca048cc0eec4d14d23ef9e3fccd431c9d
kaixuan <kaixuant@yahoo.com>
2022-01-18 17:18:05 +0800

    create likes app

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+class Tag (models.Model):
+    label = models.CharField(max_length=255)
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # generic way to identify an object, 
+    # Type (product, video, article)
+    # ID
+    # with these two attribute, we can find any data inside any application or database
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveBigIntegerField()
+    content_ibjecct = GenericForeignKey()
