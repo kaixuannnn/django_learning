01095ae6a72369069761483c33c670458d009a4e
kaixuan <kaixuant@yahoo.com>
2022-01-18 17:32:21 +0800

    make migration

--- store/models.py
+++ store/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     #sku =  models.CharField(max_length=10, primary_key=True)
     title =  models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     #when there is update in Product Model, django automatically ipdate the last_update datetime
     last_update = models.DateField(auto_now=True)
