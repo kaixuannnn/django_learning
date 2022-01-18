de35f90fcba752056f574f66108fb66f0e6c4c0f
kaixuan <kaixuant@yahoo.com>
2022-01-18 15:32:18 +0800

    order models added

--- store/models.py
+++ store/models.py
@@ -20,10 +20,24 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER,'Silver'),
         (MEMBERSHIP_GOLD, 'Gold')
     ]
-    
+
     first_name = models.CharField(max_length=255)
     last_name= models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE= 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING,'Pending'), 
+        (PAYMENT_STATUS_COMPLETE, 'Complete'), 
+        (PAYMENT_STATUS_FAILED,'Failed')
+    ]
+
+    place_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
