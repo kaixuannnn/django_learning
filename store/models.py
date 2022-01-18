0446d4d3b1c998a6bfa581f17eb0290d7bcbd620
kaixuan <kaixuant@yahoo.com>
2022-01-18 15:16:11 +0800

    field

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Product(models.Model):
+    #Django will always automatically create a primary key for us, as we want to crate key by our own can do as below
+    #sku =  models.CharField(max_length=10, primary_key=True)
     title =  models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
